4fe036742c83f2fb4d4558bbf2e7a8e7bb4c26a2
Tech Boy <93788863+DREEMGAN@users.noreply.github.com>
2021-12-06 13:05:20 +0530

    Online_Medicine_Guide

---
+++ cusfindambulence.php
@@ -0,0 +1,291 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="..........................................................................." />
+    <meta name="description" content="........................................................................" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico..........................................." />
+    <link rel="apple-touch-icon" type="image/x-icon" href="apple-touch-icon.png..............................." />
+    <title> cov </title>
+    <link rel="shortcut icon" href="img/Graphicloads-Medical-Health-Medicine-box-2.ico">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <script type="text/javascript" src="js/modernizr.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 25%;
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+    
+</head>
+
+
+<body class="cusfamb">
+
+    <div class="">
+        <div class="header-top">
+            <div class="container">
+                <a href="cupanel.php"><img src="img/client-1295901_960_720.png" style="max-height: 5%;max-width: 5%;margin-left: 50%;opacity:1.0;"></a>
+                <label class="text-center" style="margin-left:51%;"><?php echo $_SESSION["uname"];?></label> 
+                <div class="menu col-md-5" style="margin-left: 20%;margin-top: 2%">
+                    <ul class="list-unstyled list-inline pull-right">
+                        <li><a href="cushome.php">Home</a></li>
+                        <li><a href="cart/index.php">Cart</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+    <body>
+    <!--<a href="https://wa.me/918978632456" class="whatsapp_float" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i><a>-->
+    
+
+<h2>COVID GUIDELINES</h2>
+
+<div class="row">
+  <div class="column" style="background-color:#aaa;">
+    <h3>STEPS TO AVOID COVID: </h3>
+   <ul>
+       <li>Maintain a safe distance from others even if they dont appear to be sick</li>
+       <li>Wear a mask in public, especially indoors or when physical distancing is not possible</li>
+       <li>Clean your hands often. Use soap and water, or an alcohol-based hand rub</li>
+       <li>Get vaccinated when its your turn.Follow local guidance about vaccination</li>
+       <li>Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze</li>
+       <li>Stay home if you feel unwell  <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html"> More Guidelines</a></li>
+      
+</ul>
+
+  </div>
+  <div class="column" style="background-color:#bbb;">
+    <h3>SYMPTOMS OF COVID:</h3>
+    <h5>Most Common Symptoms:</h5>
+    <ul>
+        <li>fever,cough,tiredness,loss of taste or smell</li>
+        
+</ul>
+<h5>Less Common Symptoms:</h5>
+    <ul>
+        <li>sore throat,headache,diarrhoea</li>
+        <li>a rash on skin, or discolouration of fingers </li>
+        <li>red or irritated eyes</li>     
+</ul>
+<h5>Serious Symptoms:</h5>
+    <ul>
+        <li>difficult breathing or shortness of breath</li>
+        <li>loss of speech or mobility or confusion,chest pain  <a herf="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html">More guidelines</a></li>
+
+</ul>
+  </div>
+  <div class="column" style="background-color:#ccc;">
+    <h3>STEPS TO RECOVER FROM COVID:</h3>
+    <ul>
+        <li>Self isolate and stay calm . As soon as you have tested positive for the corornavirus you must self-isolate as soon ass possible</li>
+        <li>Keep regular monitoring</li>
+        <li>Seek medical attention</li>
+        <li>Eat only healthy foods and not take junk food</li>
+        <li>Dont smoke and drink alcohol</li>
+        <li>Must sleep 8 hours meainly at night time</li>
+        <a href="https://www.pathkindlabs.com/what-do-immediately-after-you-test-positive-covid-19"> More guidelines<a>
+        </ul>
+  </div>
+  <div class="column" style="background-color:#ddd;">
+    <h3>VACCINE GUIDELINES </h3>
+   <h4> Types of Vaccines</h4>
+   <ul>
+   <li>Covaxin-2 doses with interval of four weeks</li>
+   <li>Covishield-2 doses with interrval of eight weeks</li>
+   <li>Sputnik</li>
+       <li>Zydas</li>
+       <br>
+       <br>
+       <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/expect.html">More guideliness</a>
+</ul>
+  </div>
+</div>
+<a href="https://wa.me/919013151515" style="display: table; font-family: sans-serif; text-decoration: none; margin: 1em auto; color: #fff; font-size: 0.9em; padding: 1em 2em 1em 3.5em; border-radius: 2em; font-weight: bold; background: #25d366 url('https://tochat.be/click-to-chat-directory/css/whatsapp.svg') no-repeat 1.5em center; background-size: 1.6em;">For more guidelines chat here</a>
+        <a href="https://wa.me/911122307145" style="display: table; font-family: sans-serif; text-decoration: none; margin: 1em auto; color: #fff; font-size: 0.9em; padding: 1em 2em 1em 3.5em; border-radius: 2em; font-weight: bold; background: #25d366 url('https://tochat.be/click-to-chat-directory/css/whatsapp.svg') no-repeat 1.5em center; background-size: 1.6em;">For more guidelines chat here</a>
+
+
+<!--
+ <style>
+    /* for desktop */
+.whatsapp_float {
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+        font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+        z-index:100;
+}
+
+.whatsapp-icon {
+	margin-top:16px;
+}
+/* for mobile */
+@media screen and (max-width: 767px){
+     .whatsapp-icon {
+	 margin-top:10px;
+     }
+    .whatsapp_float {
+        width: 40px;
+        height: 40px;
+        bottom: 20px;
+        right: 10px;
+        font-size: 22px;  
+
+    }
+}
+
+
+
+    </style>-->
+</body>
+
+    <div class="main-area">
+
+
+
+
+        <div class="">
+            <form class="col-md-4 col-sm-offset-4 text-center" style="margin: 2%;text-align: center;margin-left: 35%;padding-top: 2%;padding-bottom: 2%;" method="post" action=cusfindambulence2.php >
+                <h2>Patient's Panel - Find covid-19 precation medicines</h2>
+                <img src=""
+
+                <br>
+               
+
+                <br>
+                <br>
+                <div class="form-group center">
+                    
+                <div class="form-group center">
+                    <input style="width:250px;height:35px;background:transparent;border:1px solid black;" type="text" name="search" placeholder="Search Ambulance from region">
+
+                    <button type="submit" style="background:#56CDF0;border:1px solid #56CDF0;padding:6px 20px;border-radius:5px;" class="btn btn-default">Search</button>
+                    <br>
+                </div>
+    
+		
+		<div class="result_table" style="margin-left: 2%;text-align: center">
+            <table style="background:white;border:1px solid black;" class="text-center">
+                <thead>
+                    <tr>
+                        <th style="border:1px solid black">medicine name</th>
+                        <th style="border:1px solid black">Region of medicine</th>
+                        <th style="border:1px solid black">medicine availability Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                      {
+//                          $str1=$_POST["search"];
+                          $con=mysqli_connect("localhost","root","");
+                        // Make sure we connected successfully
+                        if(! $con)
+                        {
+                            die('Connection Failed'.mysql_error());
+                        }
+
+                        // Select the database to use
+                        mysqli_select_db($con,'medicineguide');
+
+                        $result = mysqli_query($con,"SELECT * FROM ambulance;") or die("Failed to fetch".mysql_error());
+
+                        //$row = mysqli_fetch_array($result);
+                        
+                           if( mysqli_num_rows( $result)==0 ){
+                            echo '<tr><td colspan="3">No Rows Returned</td></tr>';
+                        }
+                    
+                        else{
+                        while( $row = mysqli_fetch_assoc( $result ) ){
+                            echo "<tr><td>{$row['hospitalName']}</td><td>{$row['amRegion']}</td><td>{$row['hospitalAddress']}</td></tr>\n";
+                        }
+                    }
+                      }
+                         
+                        
+                        
+                ?>
+                
+                </tbody>
+            </table>
+                </div>
+            </div>
+            </form>
+
+            <br>
+
+
+        
+        <!--
+		<div class="signup col-sm-offset-8 col-md-8"> 
+			<p>Not a registered user. Please Sign Up...</p>
+			<button type="submit" class="btn btn-success">  </button>
+			<button type="submit" class="btn btn-success"></button>
+			<a href="cussignup.html">As Customer</a>
+			
+		</div>
+		-->
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </div>
+
+</body>
+
+</html>
